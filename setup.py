efab96c3d1f086bfdbeca1a12d1ba2c3ef1f39a3
Krzysztof Chojnacki <kchojnacki@edu.cdv.pl>
2023-01-21 00:09:57 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Here you can convert currency of choice to PLN and check actual currency price.',
+    author='Chojnacki Krzysztof',
+    license='',
+)
